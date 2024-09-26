134ac11de056f8d8b42569a83770ad65cbd6a880
Given28 <168072758+Given28@users.noreply.github.com>
2024-09-26 13:41:26 +0200

    Add project files.

---
+++ Assignment/Program.cs
@@ -0,0 +1,48 @@
+﻿using Assignment;
+
+namespace Assignment
+{
+    class BeerSong
+    {
+        public void Sing()
+        {
+            for (int bottles = 99; bottles > 0; bottles--)
+            {
+                {
+                    if (bottles > 1)
+
+                    {
+
+                        Console.WriteLine($"{bottles} bottles of beer on the wall, {bottles} bottles of beer.");
+                        Console.WriteLine("Take one down and pass it around,");
+                        Console.WriteLine($"{bottles - 1} bottles of beer on the wall.");
+                    }
+                    else
+
+                    {
+                        Console.WriteLine($"{bottles} bottles of beer on the wall, {bottles} bottles of beer.");
+                        Console.WriteLine($"Take one down and pass it around,");
+
+
+                    }
+                    Console.WriteLine();
+
+                }
+            }
+            Console.WriteLine($"No more bottles of beer on the wall no more bottles of beer");
+            Console.WriteLine("Go to the store and buy some more, 99 bottles of beer on the wall");
+        }
+
+
+    }
+    }
+    
+    class program
+    {
+        static void Main(string[] args)
+        {
+            BeerSong beersong = new BeerSong();
+            beersong.Sing();
+        }
+    }
+
